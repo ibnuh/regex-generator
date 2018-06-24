dfd9404722c3342ac0c920bddad6c63c792dbbb0
Muhammad Ibnuh <ibnuhdev@gmail.com>
2018-06-24 16:28:43 +0700

    reset, duplicate check, alert

--- source/js/app.js
+++ source/js/app.js
@@ -6,20 +6,58 @@ new Vue({
     data: {
         input: '',
         strings: [],
-        regex: '/none/'
+        regex: '',
+        alert: {
+            displayed: false,
+            message: '',
+        }
     },
     watch: {
         strings(){
+            if(!this.strings.length){
+                this.regex = '';
+                return;
+            }
+
             this.regex = regexgen(this.strings).toString();
         }
     },
     methods: {
+        reset(){
+            var r = confirm('Are you sure to reset everything?');
+
+            if(r){
+                this.strings = [];
+                this.regex = '';
+            }
+        },
         pushString(){
-            this.strings.push(this.input);
-            this.input = '';
+            if(this.input && this.checkDuplicate()){
+                this.strings.push(this.input);
+                this.input = '';
+            }
+        },
+        checkDuplicate(){
+            var duplicate = this.strings.includes(this.input);
+
+            if(duplicate){
+                return this.showAlert('String already in added string list');
+            }
+
+            return true;
         },
         removeString(index){
             this.strings.splice(index, 1);
+        },
+        showAlert(message){
+            this.alert = {
+                displayed: true,
+                message: message,
+            }
+
+            setTimeout(() => {
+                this.alert.displayed = false;
+            }, 3000);
         }
     }
 });
